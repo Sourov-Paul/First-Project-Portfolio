f65bf7885d84450e19b32cb76eadfbfa676aac91
Sourov-Paul <sourovpaul583@gmail.com>
2022-06-01 22:22:32 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home';
+
+function App() {
+  return (
+    <div >
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react';
+import GoMap from '../GoMap/GoMap';
+import './Home.css';
+const Home = () => {
+
+    
+    return (
+        <div>
+         <GoMap></GoMap>
+
+       
+         
+        </div>
+    );
+};
+
+export default Home;
